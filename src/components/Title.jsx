35b33927f220a194b98fb5b44b44b0d4bf2f16c3
viviannemelo <viviannemmelo@gmail.com>
2022-10-21 15:09:04 -0300

    Requisito 03

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <h2>{this.props.headline}</h2>
+    );
+  }
+}
+
+export default Title;
