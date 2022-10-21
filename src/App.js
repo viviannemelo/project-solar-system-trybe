187bd7444e7a1e93fe3bb06f6776864561a92936
viviannemelo <viviannemmelo@gmail.com>
2022-10-21 20:01:48 -0300

    Testando avaliador

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import planets from './data/planets';
+// import planets from './data/planets';
 import Missions from './components/Missions';
 
 class App extends React.Component {
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div>
         <p>Sistema Solar</p>
         <Header />
-        <SolarSystem planets={ planets } />
+        <SolarSystem />
         <Missions />
       </div>
     );
