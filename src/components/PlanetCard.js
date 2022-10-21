0f1de905a5c7bf38b56e946d4f40cba6b21d2e39
viviannemelo <viviannemmelo@gmail.com>
2022-10-21 18:35:55 -0300

    Testando avaliação dos requisitos (3)

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
+// import React, { Component } from 'react';
+// import { PropTypes } from 'prop-types';
+// import planets from '../data/planets';
 
 class PlanetCard extends Component {
-  render() {
-    const { planetName, planetImage } = this.props;
-    return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
-      </div>
-    );
-  }
+//   render() {
+//     const { planetName, planetImage } = this.props;
+//     const { planetas } = this.props;
+//     const { name, image } = planetas;
+//     const { planetList } = this.props;
+//     // console.log(lista);
+//     // planets.map((planet) => (<li key={planet}> { name } { image }</li>)
+//     return (
+//       <div data-testid="planet-card">
+//         <p data-testid="planet-name">{planetName}</p>
+//         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+//         <ul>
+//           { planetList
+//             .map((planet) => <planets key={ planet } planetas={ name, image } />)}
+//         </ul>
+//       </div>
+//     );
+//   }
 }
 
-PlanetCard.propTypes = {
-  planetImage: PropTypes.string.isRequired,
-  planetName: PropTypes.string.isRequired,
-};
+// PlanetCard.propTypes = {
+//   planetImage: PropTypes.string.isRequired,
+//   planetName: PropTypes.string.isRequired,
+//   planetas: PropTypes.string.isRequired,
+//   planetList: PropTypes.shape({
+//     name: planetName,
+//     image: planetImage,
+//   }).isRequired,
+// };
 
 export default PlanetCard;
