44144b35194ec8a2389b61f536f45d2d1d9cd2f9
viviannemelo <viviannemmelo@gmail.com>
2022-10-21 19:56:07 -0300

    Requisito 07

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import planets from './data/planets';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <p>Sistema Solar</p>
         <Header />
         <SolarSystem planets={ planets } />
+        <Missions />
       </div>
     );
   }
