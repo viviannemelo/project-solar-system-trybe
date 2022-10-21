9aec8562573d0fa690b2878e3b9ff764282b707a
viviannemelo <viviannemmelo@gmail.com>
2022-10-21 14:59:50 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <div>
+        <p>Sistema Solar</p>
+        <Header />
+      </div>
+    );
   }
 }
 
 export default App;
-
